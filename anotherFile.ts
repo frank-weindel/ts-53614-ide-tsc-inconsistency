70363d29d2e4cd21a303e117970ee1869a209370
Frank Weindel <frank_weindel@comcast.com>
2023-03-31 17:06:20 -0400

    Remove anotherFile.ts

--- anotherFile.ts
+++
@@ -1,7 +0,0 @@
-import { createWebGLContext } from "./createWebGLContext.js";
-
-class CodeIsArbitrary {
-  constructor() {
-    createWebGLContext(document.getElementsByTagName("canvas")[0]!);
-  }
-}
